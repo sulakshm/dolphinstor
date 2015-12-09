a5b31e77424aa871cd98cca3b0ad31cdd3e795a2
Lakshmi Narasimhan Sundararajan <sulakshm@users.noreply.github.com>
2015-12-09 15:47:07 +0530

    Delete fixuprepo.sh

--- fixuprepo.sh
+++
@@ -1,7 +0,0 @@
-sed -i 's/ms_bind_ipv6\(.*\)/#ms_bind_ipv6\1/' /opt/ceph-cluster/ceph.conf
-sed -i "s/mon_host = \(.*\)/mon_host = ${1}/" /opt/ceph-cluster/ceph.conf
-
-cat >> /opt/ceph-cluster/ceph.conf << EOF
-osd pool default size = 2
-public_addr=${1}
-EOF
