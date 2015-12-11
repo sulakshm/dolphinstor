564e6564d694a1ab108d3af3db8f55f7eb0b7ee0
lns <lns.myspace@gmail.com>
2015-12-11 07:48:01 -0500

    Create cephadm/demo users for installation.
    Signed-off-by: lns<lns.myspace@gmail.com>

--- demo-admin.tf
+++ demo-admin.tf
@@ -16,7 +16,7 @@ resource "digitalocean_droplet" "demo-admin" {
 
   provisioner "remote-exec" {
     inline = [
-        "mkdir -p /opt/scripts",
+        "mkdir -p /opt/scripts /srv/salt /srv/pillar /srv/salt/users/cephadm/keys /srv/salt/users/demo/keys",
     ]
   }
 
@@ -41,6 +41,14 @@ resource "digitalocean_droplet" "demo-admin" {
       "sudo systemctl enable salt-master",
       "/opt/scripts/fixsaltmaster.sh ${digitalocean_droplet.demo-admin.ipv4_address_private}",
       "sudo systemctl start salt-master",
+      "sudo useradd -m cephadm",
+      "echo \"cephadm:c3ph@dm1n\" | chpasswd",
+      "su -c 'cat /dev/zero | ssh-keygen -t rsa -N \"\" ' cephadm",
+      "sudo cp /home/cephadm/.ssh/id_rsa.pub /srv/salt/users/cephadm/keys/key.pub",
+      "sudo useradd -m demo",
+      "echo \"demo:demo\" | chpasswd",
+      "su -c 'cat /dev/zero | ssh-keygen -t rsa -N \"\" ' demo",
+      "sudo cp /home/demo/.ssh/id_rsa.pub /srv/salt/users/demo/keys/key.pub",
       "sudo yum install -y ceph-deploy",
       "sudo yum install -y ntp ntpdate ntp-doc",
     ]
---
+++ scripts/salt/salt/top.sls
@@ -0,0 +1,4 @@
+base:
+    "*":
+        - users.sls
+        - groups.sls
