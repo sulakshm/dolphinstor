492f2e3b3fb0f12d816bcb1e396ab096a5bf3719
lns <lns.myspace@gmail.com>
2015-12-17 03:14:39 -0500

    Adding minor comments to mkdemo script and created a log file for caching script outputs during run.
    Signed-off-by: lns<lns.myspace@gmail.com>

--- mkdemo.sh
+++ mkdemo.sh
@@ -6,7 +6,16 @@ sed -i.bak "s/\(.*\)count=.*/\1count=${MASTER}/" demo-master.tf
 sed -i.bak "s/\(.*\)count=.*/\1count=${MINIONS}/" demo-minion.tf
 
 echo "Ensure terraform.tfvars is updated with right DO credentials"
-terraform apply
+echo "Logs are at make.out"
+echo "Creating a cluster with 1 Admin, $MASTER masters and $MINIONS minion nodes"
+
+rc=$(terraform apply 2>&1 > make.out)
+
+if [ "$rc" == "" ]; then
+  echo "cluster creation OK."
+else
+  echo "cluster creation FAIL($rc). See make.out for details."
+fi
 
 for i in `ls *.bak`
 do
